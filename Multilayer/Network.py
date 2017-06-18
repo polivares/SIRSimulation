ca149a33f1e4f53b32589b76680e9648f6243bcd
Patricio Olivares <paolivaresr@gmail.com>
2017-06-18 02:21:20 -0400

    Primeras funcionalidades de clase Network. Se crea clase SIR para modelar SIR

--- Multilayer/Network.py
+++ Multilayer/Network.py
@@ -1,11 +1,12 @@
 import multinetx as mx
 
 class Network:
-    def __init__(self,n=0, ntype = 'erdos',options={}):
-        self._N = n
-        self._NType = ntype
-        if self._NType == 'erdos':
-            self._graph = mx.generators.erdos_renyi_graph(self._N, options["rand"], seed=options["seed"])
+    def __init__(self,n=0, ntype = 'erdos', layer = 0, options={}):
+        self._layer = layer
+        self._n = n
+        self._ntype = ntype
+        if self._ntype == 'erdos':
+            self._graph = mx.generators.erdos_renyi_graph(self._n, options["rand"], seed=options["seed"])
         self._adjacency_matrix = mx.adjacency_matrix(self._graph,weight="weight")
 
     def getAdjMatrix(self):
